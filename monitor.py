134a0007bd2a66a48a77630c82c3238c9720ba89
Lars Wallenborn <lars@wallenborn.net>
2017-05-08 01:21:35 +0200

    make monitor.py importable

--- monitor.py
+++ monitor.py
@@ -5,9 +5,6 @@ from os.path import join
 import smtplib
 from email.mime.text import MIMEText
 
-base_path = os.environ['KWM_DATA_DIRECTORY']
-searx_url = os.environ['KWM_SEARX_URL']
-
 
 class Source(object):
     def query(self, keyword):
@@ -20,6 +17,9 @@ class Sink(object):
 
 
 class Searx(Source):
+    def __init__(self, searx_url):
+        self.searx_url = searx_url
+
     def query(self, keyword):
         params = {
             'q': '"%s"' % keyword,
@@ -27,7 +27,7 @@ class Searx(Source):
             'time_range': 'day',
             'format': 'json',
         }
-        r = requests.get(searx_url, params=params)
+        r = requests.get(self.searx_url, params=params)
         res = r.json()
         if len(res['results']) == 0:
             return []
@@ -55,31 +55,35 @@ class Email(Sink):
         server.quit()
 
 
-sources = [Searx()]
-sinks = [Email()]
-
-
 def ensure_and_get_path(tasking, keyword):
     path = join(base_path, tasking, 'responses')
     if not os.path.exists(path): os.mkdir(path)
     return join(path, hashlib.sha256(keyword).hexdigest())
 
 
-def get_known_urls(tasking, keyword):
-    path = ensure_and_get_path(tasking, keyword)
+def get_known_urls(tasking):
+    path = join(base_path, tasking, 'responses')
     if not os.path.exists(path): return []
-    return [url.strip() for url in open(path, 'r').read().strip().split('\n') if url.strip()]
+    ret = []
+    for file in os.listdir(path):
+        ret += [url.strip() for url in open(os.path.join(path, file), 'r').read().strip().split('\n') if url.strip()]
+    return ret
 
 
-for source in sources:
-    for tasking in os.listdir(base_path):
-        email_address = open(join(base_path, tasking, 'info.txt'), 'r').read().strip()
-        keywords = open(join(base_path, tasking, 'keywords.txt'), 'r').read().strip().split('\n')
-        for keyword in keywords:
-            known_urls = set(get_known_urls(tasking, keyword))
-            current_urls = set(source.query(keyword))
-            new_urls = list(current_urls - known_urls)
-            if new_urls:
-                open(ensure_and_get_path(tasking, keyword), 'a').write(''.join(['%s\n' % url for url in new_urls]))
-                for sink in sinks:
-                    sink.notify(keyword, email_address, new_urls)
+if __name__ == "__main__":
+    base_path = os.environ['KWM_DATA_DIRECTORY']
+    sources = [Searx(os.environ['KWM_SEARX_URL'])]
+    sinks = [Email()]
+
+    for source in sources:
+        for tasking in os.listdir(base_path):
+            known_urls = set(get_known_urls(tasking))
+            email_address = open(join(base_path, tasking, 'info.txt'), 'r').read().strip()
+            keywords = open(join(base_path, tasking, 'keywords.txt'), 'r').read().strip().split('\n')
+            for keyword in keywords:
+                current_urls = set(source.query(keyword))
+                new_urls = list(current_urls - known_urls)
+                if new_urls:
+                    open(ensure_and_get_path(tasking, keyword), 'a').write(''.join(['%s\n' % url for url in new_urls]))
+                    for sink in sinks:
+                        sink.notify(keyword, email_address, new_urls)
